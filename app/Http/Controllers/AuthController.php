74d2f6c91f1f1e770f452ce14a3c6e932d848ff4
nasywanfarid <nasywanfarid@gmail.com>
2021-10-01 20:58:46 +0700

    Migrate

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,12 +6,85 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function me(){
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
         return[
-            'nis'=>3103119138,
-            'name'=>'Nasywan',
-            'phone'=>'6282138135246',
-            'class'=>'XII RPL 4'
+            "NIS" => '3103119138',
+            "Name" => 'Nasywan Farid',
+            "Gender" => 'Laki-laki',
+            'Phone' => '082134807424',
+            "Class" => 'XII RPL 4',
         ];
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
